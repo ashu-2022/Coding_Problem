7accd46d2c32b5a1cde449559c4db0d031b11108
Ashutosh Pandit <66429348+ashu-2022@users.noreply.github.com>
2021-09-05 16:24:42 +0530

    Create Middle_Linked_List.cpp

---
+++ Linked List/Middle_Linked_List.cpp
@@ -0,0 +1,30 @@
+/*
+Middle of the Linked List                                           
+[https://leetcode.com/problems/middle-of-the-linked-list/]
+Approach :
+    1. By Transversing linked list 2 times(or passes)
+        1st time to count no of node, 
+        2nd time visit (N/2) times to find middle node
+        T(n) = O(2N) and S(n) = O(1)
+
+    2. By using slow,fast pointer in single pass
+        T(n) = O(N) and S(n) = O(1)
+
+*/
+
+
+
+// Middle of the Linked List
+// Give middle node in right part in case of even nodes
+// T(n) = O(N) and S(n) = O(1)
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        ListNode *slow=head, *fast=head;
+        while(fast && fast->next){
+            slow=slow->next;
+            fast=fast->next->next; 
+        }
+        return slow;
+    }
+};
