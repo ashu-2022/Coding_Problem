9056e1fc0be24d7df40e658b5771bb323e314064
Ashutosh Pandit <66429348+ashu-2022@users.noreply.github.com>
2021-09-08 21:06:20 +0530

    Update Inorder Traversal.py

--- Tree/Inorder Traversal.py
+++ Tree/Inorder Traversal.py
@@ -9,12 +9,6 @@ Approach:
         T(n) = O(N) and S(n) = O(N)
 
 """
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
 
 
 # Recursive Approach
